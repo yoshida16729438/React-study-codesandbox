3bb6d943a1120af592b1d07569767355d21c54a6
yoshida16729438 <33854588+yoshida16729438@users.noreply.github.com>
2023-09-27 00:29:55 +0000

    Update 3 files ひとまずReactが動く

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>お元気ですか</p>
+    </>
+  );
+};
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDom.render(<App />, document.getElementById("root"));
