8e5ea5bb8b432bc0da5f0e8a40439db71ad360a3
yoshida16729438 <33854588+yoshida16729438@users.noreply.github.com>
2023-09-28 00:33:18 +0000

    Update App.jsx useStateを使用した状態管理と画面の書き換え

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/colorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => setNum(num + 1);
+  //状態を示す数値numと状態を更新するための関数setNumをuseStateから分割代入
+  //useState(0)の0はnumの初期値指定
+  const [num, setNum] = useState(0);
+  //数値型である必要性はなし
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickSwitchShowFlag = () => setFaceShowFlag(!faceShowFlag);
+
   const contentStyle = {
     color: "blue",
     fontSize: "18px", //通常のCSSならfont-sizeだがReact用はキャメルケースで命名しなおされている
   };
   return (
     <>
-      <h1 style={{ color: "red" }}>Hello world</h1>
       {/*JSを示す{}の中にオブジェクトを示す{}がある*/}
-      <p style={contentStyle}>お元気ですか</p>
+      <h1 style={{ color: "red" }}>Hello world</h1>
       {/*JSを示す{}があり、その中でオブジェクトを指定*/}
-      <button onClick={onClickButton}>ボタン</button>
+      <p style={contentStyle}>お元気ですか</p>
       {/*onclickイベントもキャメルケースで命名されている*/}
+      <button onClick={onClickCountUp}>カウントアップ</button>
+
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <ColorfulMessage color="pink" message="私は">
         元気です
       </ColorfulMessage>
+      <p>{num}</p>
+      {faceShowFlag && <p>(^^)</p>}
     </>
   );
 };
